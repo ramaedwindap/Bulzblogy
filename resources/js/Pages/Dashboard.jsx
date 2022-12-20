5e1f8f398c50014d1a248e6af553f4f42fd9c05f
Rama Edwinda Putra <ramaedwindap@gmail.com>
2022-12-20 09:23:23 +0700

    membuat container, dashboard

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,20 +1,13 @@
 import { Head } from "@inertiajs/inertia-react";
 import App from "@/Layouts/App";
+import React from "react";
+import Container from "@/Components/Container";
 
 export default function Dashboard(props) {
     return (
         <>
             <Head title="Dashboard" />
-
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">
-                            You're logged in!
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Container>Dashboard</Container>
         </>
     );
 }
