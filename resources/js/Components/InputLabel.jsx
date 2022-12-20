c070970417b29cca371d7fa13807947ad8eb6d40
Rama Edwinda Putra <ramaedwindap@gmail.com>
2022-12-20 08:52:04 +0700

    menambahkan margin bottom

--- resources/js/Components/InputLabel.jsx
+++ resources/js/Components/InputLabel.jsx
@@ -1,6 +1,11 @@
 export default function InputLabel({ forInput, value, className, children }) {
     return (
-        <label htmlFor={forInput} className={`block font-medium text-sm text-gray-700 ` + className}>
+        <label
+            htmlFor={forInput}
+            className={
+                `block font-medium mb-1 text-sm text-gray-700 ` + className
+            }
+        >
             {value ? value : children}
         </label>
     );
